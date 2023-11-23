4cf3de80e0b49a076b6a66e4e84160790cd47e12
Carlos-Lozano-A <151076361+Carlos-Lozano-A@users.noreply.github.com>
2023-11-23 17:35:14 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,8 @@
+function calculateNumbers(var1, var2) {
+    const sumTotal = var1 + var2;
+    const diffTotal = var1 - var2;
+    const multTotal = var1 * var2;
+    const divTotal = var1 / var2;
+
+    return [sumTotal, diffTotal, multTotal, divTotal];
+}
